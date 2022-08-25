d7b88605ca26972237c214d5eeed2fe45bb1fb9a
Hendrik Mans <hendrik@mans.de>
2022-08-25 11:50:00 +0200

    Simplify Fresnel, it should always overwrite color

--- packages/material-composer/src/modules/Fresnel.ts
+++ packages/material-composer/src/modules/Fresnel.ts
@@ -1,9 +1,10 @@
-import { Add, Fresnel as FresnelUnit, FresnelProps } from "shader-composer"
+import { Fresnel as FresnelUnit, FresnelProps, Mul } from "shader-composer"
+import { Color } from "three"
 import { ModuleFactory } from ".."
 
 export type FresnelArgs = FresnelProps
 
 export const Fresnel: ModuleFactory<FresnelArgs> = (props) => (state) => ({
   ...state,
-  color: Add(state.color, FresnelUnit(props))
+  color: Mul(new Color("white"), FresnelUnit(props))
 })
