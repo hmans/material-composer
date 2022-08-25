45beed625b49334f8261592dad484dac4137cbc4
Hendrik Mans <hendrik@mans.de>
2022-08-25 11:27:47 +0200

    no longer memo modules

--- packages/material-composer-r3f/src/reactor.tsx
+++ packages/material-composer-r3f/src/reactor.tsx
@@ -1,13 +1,16 @@
 import { Module, ModuleFactory, ModuleFactoryProps } from "material-composer"
 import * as Modules from "material-composer/modules"
 import { FC, memo, useMemo } from "react"
+import { Input } from "shader-composer"
 import { useModuleRegistration } from "./moduleRegistration"
 
 type Modules = typeof Modules
 
 const cache = new Map<string, ModuleComponent<any>>()
 
-type ModuleComponentProps<K extends keyof Modules> = Parameters<Modules[K]>[0]
+type ModuleComponentProps<K extends keyof Modules> = Parameters<
+  Modules[K]
+>[0] & { blend?: Input<"float"> }
 
 type ModuleComponent<K extends keyof Modules> = FC<ModuleComponentProps<K>>
 
@@ -19,12 +22,13 @@ type ModuleComponentProxy = {
 
 export const makeModuleComponent = <P extends ModuleFactoryProps>(
   fac: ModuleFactory<P>
-) =>
-  memo((props: P) => {
-    const module = useMemo(() => fac(props), [props])
-    useModuleRegistration(module)
-    return null
-  })
+) => (props: P) => {
+  const module = useMemo(() => fac(props), [props])
+
+  useModuleRegistration(module)
+
+  return null
+}
 
 export const ModuleReactor = new Proxy<ModuleComponentProxy>(
   {} as ModuleComponentProxy,
