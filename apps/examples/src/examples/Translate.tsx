053087152db0639e6eca8d9cf8833005b404f4aa
Hendrik Mans <hendrik@mans.de>
2022-08-26 21:26:15 +0200

    Play with spaces

--- apps/examples/src/examples/Translate.tsx
+++ apps/examples/src/examples/Translate.tsx
@@ -1,19 +1,28 @@
+import { useControls } from "leva"
 import { ComposableMaterial, Modules } from "material-composer-r3f"
+import { Space } from "material-composer/modules"
 import { Description } from "r3f-stage"
 import { useMemo } from "react"
 import { Mul, Sin, Time } from "shader-composer"
 import { Vector3 } from "three"
 
 export default function Translate() {
+  const controls = useControls({
+    space: { value: "world", options: ["local", "world", "view"] }
+  })
+
   const time = useMemo(() => Time(), [])
 
   return (
     <group>
-      <mesh position-y={1.5}>
+      <mesh position-y={1.5} rotation-z={Math.PI / 2}>
         <sphereGeometry />
 
         <ComposableMaterial>
-          <Modules.Translate offset={Mul(new Vector3(1, 0, 0), Sin(time))} />
+          <Modules.Translate
+            offset={Mul(new Vector3(1, 0, 0), Sin(time))}
+            space={controls.space as Space}
+          />
         </ComposableMaterial>
       </mesh>
 
