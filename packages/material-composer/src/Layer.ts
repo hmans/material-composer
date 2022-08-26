e015b02c6a0e028a122b9390c70420253f9b446b
Hendrik Mans <hendrik@mans.de>
2022-08-26 17:03:32 +0200

    Fix blending of remaining values

--- packages/material-composer/src/Layer.ts
+++ packages/material-composer/src/Layer.ts
@@ -1,11 +1,13 @@
-import { $, Input, Mix, Vec3 } from "shader-composer"
+import { $, Input, Mix, type, Unit } from "shader-composer"
 import { ModuleFactory, ModulePipe, pipeModules } from "."
 
-export type BlendFunction = (
-  a: Input<"vec3">,
-  b: Input<"vec3">,
+export type BlendableType = "float" | "vec2" | "vec3" | "vec4"
+
+export type BlendFunction = <T extends BlendableType>(
+  a: Input<T>,
+  b: Input<T>,
   opacity: Input<"float">
-) => Input<"vec3">
+) => Input<T>
 
 export type BlendMode = "normal" | "add" | "discard"
 
@@ -14,7 +16,8 @@ export type BlendMode = "normal" | "add" | "discard"
 export const Blend: Record<BlendMode, BlendFunction> = {
   normal: (a, b, f) => (f === 1 ? b : f === 0 ? a : Mix(a, b, f)),
   discard: (a) => a,
-  add: (a, b, f) => Vec3($`min(${a} + ${b}, 1.0) * ${f} + ${a} * (1.0 - ${f})`)
+  add: (a, b, f) =>
+    Unit(type(a), $`min(${a} + ${b}, 1.0) * ${f} + ${a} * (1.0 - ${f})`)
 }
 
 export type LayerArgs = {
@@ -35,7 +38,11 @@ export const Layer: ModuleFactory<LayerArgs> = ({
   const blendFunction = typeof blend === "string" ? Blend[blend] : blend
 
   return {
-    ...newState,
-    color: blendFunction(state.color, newState.color, opacity)
+    color: blendFunction(state.color, newState.color, opacity),
+    position: Blend.normal(state.position, newState.position, opacity),
+    alpha: Blend.normal(state.alpha, newState.alpha, opacity),
+    normal: Blend.normal(state.normal, newState.normal, opacity),
+    roughness: Blend.normal(state.roughness, newState.roughness, opacity),
+    metalness: Blend.normal(state.metalness, newState.metalness, opacity)
   }
 }
