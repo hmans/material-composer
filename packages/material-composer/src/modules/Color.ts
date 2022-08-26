8a3ceb942870934323a529b17706cbd37a6c4012
Hendrik Mans <hendrik@mans.de>
2022-08-26 14:47:55 +0200

    Support different blend modes in `Modules.Color` Fixes #20

--- packages/material-composer/src/modules/Color.ts
+++ packages/material-composer/src/modules/Color.ts
@@ -1,15 +1,42 @@
-import { Input } from "shader-composer"
+import { Add, Input, vec3 } from "shader-composer"
 import { Color as ColorImpl, ColorRepresentation } from "three"
 import { ModuleFactory } from ".."
 
-export type ColorArgs = {
-  color: Input<"vec3"> | ColorRepresentation
+export type BlendFunction = (
+  a: Input<"vec3">,
+  b: Input<"vec3">
+) => Input<"vec3">
+
+export type BlendMode = "normal" | "add"
+
+export const Blend: Record<BlendMode, BlendFunction> = {
+  normal: (a, b) => b,
+  add: (a, b) => Add(a, b)
 }
 
-export const Color: ModuleFactory<ColorArgs> = ({ color }) => (state) => ({
-  ...state,
-  color:
+export type ColorArgs = {
+  color: Input<"vec3"> | ColorRepresentation
+  blend?: BlendFunction | BlendMode
+}
+
+export const Color: ModuleFactory<ColorArgs> = ({
+  color,
+  blend = Blend.normal
+}) => (state) => {
+  /* Determine new color */
+  const newColor =
     typeof color === "string" || typeof color === "number"
       ? new ColorImpl(color)
       : color
-})
+
+  /* Determine blend function */
+  const blendFunction = typeof blend === "string" ? Blend[blend] : blend
+
+  /* Apply blending */
+  const blendedColor = blendFunction(state.color, newColor)
+
+  return {
+    ...state,
+    color: blendedColor
+  }
+}
