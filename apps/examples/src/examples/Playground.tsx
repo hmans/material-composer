bd236250b135986e00aa6ed3ff8ffeb57d32b5ee
Hendrik Mans <hendrik@mans.de>
2022-08-27 17:30:44 +0200

    Cool

--- apps/examples/src/examples/Playground.tsx
+++ apps/examples/src/examples/Playground.tsx
@@ -1,10 +1,38 @@
 import { patchMaterial } from "material-composer"
 import { useMemo } from "react"
+import { pipe } from "shader-composer"
 import { MeshStandardMaterial } from "three"
+
+const extend = (anchor: string) => ({
+  with: (target: string) => (source: string) =>
+    source.replace(anchor, `${anchor}\n${target}`)
+})
 
 export default function Playground() {
   const material = useMemo(() => {
-    const material = patchMaterial(new MeshStandardMaterial())
+    const material = patchMaterial(
+      new MeshStandardMaterial({ color: "hotpink" })
+    )
+
+    material.onBeforeCompile = (shader) => {
+      shader.fragmentShader = pipe(
+        shader.fragmentShader,
+
+        extend("void main() {").with(`
+          vec3 csm_DiffuseColor = diffuse;
+          float csm_Alpha = opacity;
+        `),
+
+        extend("#include <color_fragment>").with(
+          "diffuseColor = vec4(csm_DiffuseColor, csm_Alpha);"
+        ),
+
+        extend("#include <roughnessmap_fragment>").with(
+          "roughnessFactor = 0.5;"
+        )
+      )
+    }
+
     return material
   }, [])
 
